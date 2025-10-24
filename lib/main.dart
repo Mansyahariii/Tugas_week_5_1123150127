b6dcb7d6b7796c61d9029c2fb33f8fd60a3740da
Mansyahariii <arifirmansyah350@gmail.com>
2025-10-24 09:19:48 +0700

    membuat HomePage menggunakan stateless

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Latihan Container",
       debugShowCheckedModeBanner: false,
+      home: HomePage();
     );
+  }
+}
+
+//membuat page HomePage menggunakan stateless
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
